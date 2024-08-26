906b988ee936bb37368ede323aba451391e158d8
Rh4096 <rhelvetican@gmail.com>
2024-08-26 14:29:47 +0700

    restructure

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
-use paste::paste;
-use utils::input;
+use utils::get_id;
 
 mod solutions;
+
+use paste::paste;
+#[macro_use]
 mod utils;
 
-macro_rules! match_id {
-    ($obj:expr, $($id:literal), *) => {
-        match $obj {
-            $($id => println!("{}", paste!(solutions::[<p $id>]::solution()))),*,
-            _ => println!("Unimplemented problem."),
-        }
-    };
-}
-
 fn main() {
-    let mut id = input("Input problem ID: ").parse::<usize>().unwrap();
+    let mut id = get_id();
     while id != 99999 {
         match_id!(id, 1);
-        id = input("Input problem ID: ").parse::<usize>().unwrap();
+        id = get_id();
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,32 @@
+#[macro_export]
+macro_rules! match_id {
+    ($obj:expr, $($id:literal), *) => {
+        match $obj {
+            $($id => println!("{}", paste!(solutions::[<p $id>]::solution()))),*,
+            _ => println!("Unimplemented problem."),
+        }
+    };
+}
+
 use std::io::{stdin, stdout, Write};
 
-pub fn input(msg: &str) -> String {
+fn input(msg: &str) -> String {
     print!("{}", msg);
     let mut buf = String::new();
     stdin().read_line(&mut buf).unwrap();
     stdout().flush().unwrap();
     buf
+}
+
+pub fn get_id() -> usize {
+    let mut res = input("Input problem ID: ").trim().parse::<usize>();
+    while res.is_err() {
+        res = input("Input problem ID: ").trim().parse::<usize>();
+    }
+
+    if let Ok(res) = res {
+        return res;
+    }
+
+    0
 }
